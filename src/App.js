320f7d2d364f5647dbb12013e87f6579f1982db3
anuragdongre <anuragdongre555@gmail.com>
2023-04-26 23:05:18 +0530

    weather app search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
+
+const Api_key = "599da3b8dc658f8f9a384d9836d5937f";
 
 function App() {
+  const inputRef = useRef(null);
+  const fatchWeather = async () => {
+    const URL = "";
+    console.log(inputRef.current.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800 h-screen grid place-items-center">
+      <div className="bg-white w-96 p-4 rounded-md">
+        <div className="flex items-center justify-between">
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Enter Your Location "
+            className="text-xl border-b p-1 border-gray-200 font-semibold uppercase"
+          />
+          <button onClick={fatchWeather}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/758/758651.png"
+              alt="..."
+              className="w-8"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
